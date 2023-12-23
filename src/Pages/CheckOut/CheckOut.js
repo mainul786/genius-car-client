905610ce64b084c1ba93c1e594adf95855036a5c
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-12-23 22:52:23 +0530

    create

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -3,13 +3,13 @@ import { useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
 const CheckOut = () => {
-    const {_id, title, price } = useLoaderData();
-    const {user} = useContext(AuthContext);
+    const { _id, title, price } = useLoaderData();
+    const { user } = useContext(AuthContext);
 
-    const handleOrderSubmit = event =>{
+    const handleOrderSubmit = event => {
         event.preventDefault();
         const form = event.target;
-        const name = `${form.fname.value } ${form.lname.value}`;
+        const name = `${form.fname.value} ${form.lname.value}`;
         const phone = form.phone.value;
         const email = user?.email || 'unregistered';
         const message = form.message.value;
@@ -18,29 +18,31 @@ const CheckOut = () => {
         const order = {
             service: _id,
             serviceName: title,
-            price, 
+            price,
             customer: name,
             phone,
             email,
             message
         }
 
-        fetch(`http://localhost:5000/orders`,{
-            method:'POST',
-            headers:{
-                'content-type':'application/json'
+        fetch(`http://localhost:5000/orders`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                authorization: `Bearer ${localStorage.getItem('Genius-token')}`
+
             },
-            body:JSON.stringify(order)
+            body: JSON.stringify(order)
         })
-        .then(res=> res.json())
-        .then(data=> {
-            console.log(data)
-            if(data.acknowledged){
-                form.reset();
-                alert('Order SuccessFully!');
-            }
-        })
-        .catch(error =>console.log(error))
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+                    form.reset();
+                    alert('Order SuccessFully!');
+                }
+            })
+            .catch(error => console.log(error))
 
     }
 
@@ -55,8 +57,8 @@ const CheckOut = () => {
                     <input type="text" name="phone" placeholder="Your Phone" className="input input-bordered input-accent w-full " requrired />
                     <input type="text" name="email" defaultValue={user?.email} readOnly placeholder="Your Email" className="input input-bordered input-accent w-full " />
                 </div>
-                    <textarea name="message" placeholder="Your Message" className="textarea textarea-bordered textarea-lg w-full " ></textarea>
-                    <button className="btn btn-outline btn-warning w-full">Order Confirm</button>
+                <textarea name="message" placeholder="Your Message" className="textarea textarea-bordered textarea-lg w-full " ></textarea>
+                <button className="btn btn-outline btn-warning w-full">Order Confirm</button>
             </form>
         </div>
     );
--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -28,7 +28,10 @@ const Orders = () => {
     const proceed = window.confirm('are you sure, you went to cancel your Order!');
     if (proceed) {
       fetch(`http://localhost:5000/orders/${id}`, {
-        method: 'DELETE'
+        method: 'DELETE',
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('Genius-token')}`
+        }
       })
         .then(res => res.json())
         .then(data => {
@@ -47,7 +50,8 @@ const Orders = () => {
       fetch(`http://localhost:5000/orders/${id}`, {
         method:'PATCH',
         headers:{
-          'content-type':'application/json'
+          'content-type':'application/json',
+          authorization: `Bearer ${localStorage.getItem('Genius-token')}`
         },
         body:JSON.stringify({status:'Approved'})
       })  
